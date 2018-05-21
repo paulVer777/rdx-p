44deaf63776e8c93d57dfa8a0d9a10e09bc5827f
paulVer777 <pawel.gitara@onet.eu>
2018-05-21 12:05:54 +0200

    kreatory akcji

--- src/MessageList/redux.js
+++ src/MessageList/redux.js
@@ -1,38 +1,62 @@
-
 ////////////STAN KOMPONENTU//////////////
 
 
-const initialStateForMessages={
+const initialStateForMessages = {
 
-    imBusy:true,
-    messages:[],
-    newMessage:{}
+    imBusy: true,
+    messages: [],
+    newMessage: {},
+    error: '', //odpowiada za tresc errora
+    imWithError:false
 }
 
 /////////////////////////////////////////
 
 //////////// TYPY AKCJI ////////////////////
 
-const TYPES={
+const TYPES = {
 
-    ADD_MESSAGE:'ADD_MESSAGE',
-    INITIAL_PREFETCH_MESSAGES:"INITIAL_PREFETCH_MESSAGES",
-    FETCHED_MESSAGES_WITH_SUCCES:"FETCHED_MESSAGES_WITH_SUCCES",
-    FETCHED_MESSAGES_WITH_ERROR:"FETCHED_MESSAGES_WITH_ERROR"
+    ADD_MESSAGE: 'ADD_MESSAGE',
+    INITIAL_PREFETCH_MESSAGES: "INITIAL_PREFETCH_MESSAGES",
+    FETCHED_MESSAGES_WITH_SUCCES: "FETCHED_MESSAGES_WITH_SUCCES",
+    FETCHED_MESSAGES_WITH_ERROR: "FETCHED_MESSAGES_WITH_ERROR",
+
+    ADD_MESSSAGE: 'ADD_MESSAGE'
+
 }
 
 ////////////////////////////////////////
 
 ///////// KREATORY AKCJI ///////////////////
 
-const ACTIONS={
+const ACTIONS = {
 
+    initialGetMessages: () => ({
 
+        type: TYPES.INITIAL_PREFETCH_MESSAGES,
+        data: {
+            imbusy: true
+        }
+    }),
 
+    handleMessagesWithSucces: (messages) => ({
 
+        type: TYPES.FETCHED_MESSAGES_WITH_SUCCES,
+        data: {
+            imBusy: false,
+            messages
+        }
+    }),
 
+    handleMessagesWithError: (messages) => ({
 
-
+        type: TYPES.FETCHED_MESSAGES_WITH_SUCCES,
+        data: {
+            imBusy: false,
+            imWithError: true,
+            error
+        }
+    })
 
 
 }
