475ecc3d2d19eba8f80afb4020570e66199e3eb6
paulVer777 <pawel.gitara@onet.eu>
2018-05-21 11:57:02 +0200

    creating state

---
+++ src/MessageList/redux.js
@@ -0,0 +1,41 @@
+
+////////////STAN KOMPONENTU//////////////
+
+
+const initialStateForMessages={
+
+    imBusy:true,
+    messages:[],
+    newMessage:{}
+}
+
+/////////////////////////////////////////
+
+//////////// TYPY AKCJI ////////////////////
+
+const TYPES={
+
+    ADD_MESSAGE:'ADD_MESSAGE',
+    INITIAL_PREFETCH_MESSAGES:"INITIAL_PREFETCH_MESSAGES",
+    FETCHED_MESSAGES_WITH_SUCCES:"FETCHED_MESSAGES_WITH_SUCCES",
+    FETCHED_MESSAGES_WITH_ERROR:"FETCHED_MESSAGES_WITH_ERROR"
+}
+
+////////////////////////////////////////
+
+///////// KREATORY AKCJI ///////////////////
+
+const ACTIONS={
+
+
+
+
+
+
+
+
+
+}
+
+
+///////////////////////////////////////
