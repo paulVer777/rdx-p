bb73e042fa0249098c6536c73b604ea146c7fc7f
paulVer777 <pawel.gitara@onet.eu>
2018-05-21 09:56:02 +0200

    drawer added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react'
 import Button from '@material-ui/core/Button'
 
-
 import logo from './logo.svg'
 import './App.css'
 
-import {MainDrawer} from './common/Drawer'
+import MainDrawer from './common/Drawer'
 
 class App extends Component {
     render () {
         return (
             <div className="App">
                 <MainDrawer/>
-
                 <Button variant="raised" color="primary">
                     Hello World
                 </Button>
--- src/common/Drawer.js
+++ src/common/Drawer.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react'
+
+
+// HOC który pozwala wrzucić style do komponentu
+import { withStyles } from '@material-ui/core/styles';
+
 import Button from '@material-ui/core/Button'
 import Drawer from '@material-ui/core/Drawer'
 
+import List from '@material-ui/core/List';
+import MenuItem from '@material-ui/core/MenuItem';
+import Divider from '@material-ui/core/Divider';
 
-export class MainDrawer extends Component {
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+// style wg material
+const styles = theme => ({
+    drawerPaper: {
+        position: 'fixed',
+        width: 300
+    },
+});
+
+class MainDrawer extends Component {
     render () {
         return (
             <Drawer variant="persistent"
                     open={true}
                     anchor={'left'}
-                    style={{width:400}}
+                    classes={{
+                        paper:this.props.classes.drawerPaper
+                    }}
             >
                 <h1>test</h1>
             </Drawer>
         )
     }
 }
+
+export default withStyles(styles,{withTheme:true})(MainDrawer)
